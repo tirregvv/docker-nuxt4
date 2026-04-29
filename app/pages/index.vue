4b2e7cad24ee0992b41d7701b6a0baff1c944f0f
Gerrit van Vliet <45655863+tirregvv@users.noreply.github.com>
2026-04-29 23:02:55 +0200

    fix: add sub text

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="wrapper">
 		<h1>Welcome to the Nuxt App</h1>
+		<p>This is the home page of the Nuxt App.</p>
 	</div>
 </template>
 
@@ -9,17 +10,22 @@ body {
 	background-color: #0f211b;
 	margin: 0;
 	padding: 0;
+	color: #9de4cd;
+	font-family: "Arial", sans-serif;
+	text-align: center;
 }
 .wrapper {
 	display: flex;
+	flex-direction: column;
 	justify-content: center;
 	align-items: center;
 	height: 100dvh;
 }
 h1 {
-	color: #9de4cd;
-	font-family: "Arial", sans-serif;
-	text-align: center;
 	margin-top: 50px;
+}
+p {
+	font-size: 1.2em;
+	margin-top: 20px;
 }
 </style>
