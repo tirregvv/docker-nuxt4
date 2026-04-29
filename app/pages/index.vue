9bff625ac23e234e4eb67f2612d75162e5b81c2f
Gerrit van Vliet <45655863+tirregvv@users.noreply.github.com>
2026-04-29 22:47:39 +0200

    fix: added index page style

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,3 +3,17 @@
 		<h1>Welcome to the Nuxt App</h1>
 	</div>
 </template>
+
+<style>
+body {
+	background-color: #0f211b;
+	margin: 0;
+	padding: 0;
+}
+h1 {
+	color: #236964;
+	font-family: "Arial", sans-serif;
+	text-align: center;
+	margin-top: 50px;
+}
+</style>
